dbe5040796725417ac9a819c707384ba35fb0431
winklerc284 <winklerc284@gmail.com>
2023-05-11 12:13:11 +0800

    modfiy slot manager

--- contracts/base/ISolt.sol
+++ contracts/base/ISolt.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity =0.8.12;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 
-interface IStrategy {
+interface ISolt {
     
     function deposit(IERC20 token, uint256 amount) external returns (uint256);
 
