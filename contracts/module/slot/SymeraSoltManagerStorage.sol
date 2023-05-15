84e1c7f823c1915a5e86af768d03e0050673b756
winklerc284 <winklerc284@gmail.com>
2023-05-15 14:52:07 +0800

    create depositIntoSlotWithSignature method two

--- contracts/module/slot/SymeraSoltManagerStorage.sol
+++ contracts/module/slot/SymeraSoltManagerStorage.sol
@@ -9,6 +9,12 @@ import "../../base/IDelegationManager.sol";
  * @notice This storage contract is separate from the logic to simplify the upgrade process.
  */
 abstract contract SymeraSoltManagerStorage is ISlotManager {
+    /// @notice The EIP-712 typehash for the contract's domain
+    bytes32 public constant DOMAIN_TYPEHASH =
+        keccak256("EIP712Domain(string name,uint256 chainId,address verifyingContract)");
+    /// @notice The EIP-712 typehash for the deposit struct used by the contract
+    bytes32 public constant DEPOSIT_TYPEHASH =
+        keccak256("Deposit(address strategy,address token,uint256 amount,uint256 nonce,uint256 expiry)");
     /// @notice EIP-712 Domain separator
     bytes32 public DOMAIN_SEPARATOR;
     // staker => number of signed deposit nonce (used in depositIntoSlotWithSignature)
